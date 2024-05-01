2ff16be4959690157ae4df86169e4efe8f9a431b
galcta-kinght <144710665+galcta-kinght@users.noreply.github.com>
2024-05-01 15:45:30 +0000

    added tasks 2-5

--- scripts/main.js
+++ scripts/main.js
@@ -3,3 +3,27 @@ for(let i = 1; i <= 10; i++)
 {console.log(i * i);}
 
 console.log("task 2")
+for(let i=5; i >= 0; i--) {
+    console.log(i)
+}
+console.log("blast off")
+
+console.log("task 3")
+for (let i = 1; i <= 50; i+=2) {
+        console.log(i+1);
+    }
+
+
+console.log("task 4")
+let sum = 0
+for (let i = 1; i <= 100; i++) {
+    sum = sum+i
+}
+console.log(sum)
+
+console.log("task 5")
+let factorial = 1;
+for(let i=1; i<=10; i++){
+    factorial*=i;
+    console.log(factorial)
+}
