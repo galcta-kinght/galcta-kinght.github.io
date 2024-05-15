6361b6eb43ee6635dccb44f5ef5eb6a044d9909d
galcta-kinght <144710665+galcta-kinght@users.noreply.github.com>
2024-05-15 16:01:17 +0000

    fixed stuff

--- scripts/rainbow.js
+++ scripts/rainbow.js
@@ -1,9 +1,2 @@
 arr = ["red", "orange", "yellow", "green", "blue", "indago", "violet"];
 list = document.createElement('ul');
-
-console.log("colors")
-
-for (let i = 0; i < color.length; i++) {
-    colorlist = document.createElement('ul');
-    color[i].innerText
-  }
